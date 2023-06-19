d3fd87d1cd7a37b7318fc4ecfae65ecfb22f241a
Your Name <natnaewasihun512@gmail.com>
2023-06-19 02:24:19 +0200

    feat: Header component

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,10 +4,9 @@ export default function About() {
     return (
         <main className="mt-6">
             <div>
-                {/* <h1 className="text-2xl font-semibold">About</h1> */}
-                <Header>About</Header>
-                <p className="mt-5">Lorem ipsum....</p>
+            <h1 className="text-2xl font-sans">About page </h1>
+            <p className="mt-5">Lorem ipsum</p>        
             </div>
         </main>
-    );
+    )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main>
+    <main className="">
       <h1>Home</h1>
     </main>
   )
